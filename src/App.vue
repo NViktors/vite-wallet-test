3f23c89ca0e837daa1d54ee9e698d24f2361867f
Viktors <viktors97n@gmail.com>
2022-01-12 17:30:46 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <wallet-provider :auto-connect="true" :wallets="wallets">
+      <wallet-modal-provider>
+        <wallet-multi-button/>
+      </wallet-modal-provider>
+    </wallet-provider>
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper {
+  margin: 0 auto;
+  display: flex;
+  width: 80%;
+  align-content: center;
+  justify-content: center;
+  // flex-direction: column;
+}
+</style>
+
+<script setup lang="ts">
+  import {WalletProvider} from "@solana/wallet-adapter-vue";
+  import {getPhantomWallet, getSolflareWallet} from '@solana/wallet-adapter-wallets';
+  import {WalletModalProvider, WalletMultiButton} from "@solana/wallet-adapter-vue-ui";
+
+  const wallets = [
+    getPhantomWallet(),
+    getSolflareWallet()
+  ];
+</script>
+
